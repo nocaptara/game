7162a99eede58233ce95387699ec5324002bbadb
nocaptara <antara4210@gmail.com>
2023-07-14 10:55:38 +0530

    done

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const card = document.querySelectorAll('.cell')
 const front = document.querySelectorAll('.front')
+const container = document.querySelector('.container')
+const score = document.querySelector('.score span')
 
 
 
-
-suffleImage() 
+shuffleImage()
 clicking()
-
-function suffleImage(){
+function shuffleImage(){
 
 
     card.forEach(c=>{
@@ -18,15 +18,62 @@ function suffleImage(){
         c.style.order = num[random]
     })
 }
+
+
 function clicking(){
 
     for(let i =0; i<card.length; i++){
 
 
-        card[i].addEventListener('click') ,()=>{
+        front[i].classList.add('show')
+
+        setInterval(() => {
+            front[i].classList.remove('show')
+        }, 2000);
+
+        card[i].addEventListener('click' ,()=>{
 
             front[i].classList.add('flip')
+           const filppedCard = document.querySelectorAll('.flip')
+
+            if(filppedCard.length == 2){
+
+                container.style.pointerEvents ='none'
+                
+                setInterval(() => {
+                    
+                    container.style.pointerEvents ='all'
+                }, 1000);
+ 
+                match(filppedCard[0] , filppedCard[1])
+            }
+        })
     }
 }
-}
 
+
+
+
+function match(cardOne , cardTwo){
+
+    if(cardOne.dataset.index == cardTwo.dataset.index){
+
+        score.innerHTML = parseInt(score.innerHTML) + 1
+       
+        cardOne.classList.remove('flip') 
+        cardTwo.classList.remove('flip') 
+
+
+        cardOne.classList.add('match')
+        cardTwo.classList.add('match')
+
+
+    }else{
+
+        setTimeout(() => {
+            
+            cardOne.classList.remove('flip') 
+            cardTwo.classList.remove('flip') 
+        }, 1000);
+    }
+}
